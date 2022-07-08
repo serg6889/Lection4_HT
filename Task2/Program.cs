a8298d71ee8faf5fb3e88a16f92812b0f6ac74ae
Sergey68 <sesergey@yandex.ru>
2022-07-09 08:44:44 +1100

    File is updated with Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Input number   ");
+string number = Console.ReadLine()!;
+int numlength = number.Length;
+int sum = 0;
+for(int j = 0; j <numlength; j++)
+{
+sum +=(number[j] - '0'); 
+}
+Console.WriteLine($"Sum of digits in number{number} = {sum}");
