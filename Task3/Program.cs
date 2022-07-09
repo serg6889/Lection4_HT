2723cf204e03a3fd9478f858aa05db7dd376cf6b
Sergey68 <sesergey@yandex.ru>
2022-07-10 01:59:39 +1100

    File is updated with Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа задаёт массив из 8 элементов и выводит их на экран
+int[] array = new int[8] ;
+void FillArray (int[] massive) 
+{
+       for (int j=0; j < massive.Length; j++)
+      { 
+	  massive[j] = new Random().Next(9);
+	
+      }
+
+}
+void ShowMass (int[] massive) 
+{
+foreach(var item in massive)
+{
+Console.WriteLine(item + "");
+}
+}
