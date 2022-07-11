70a62f8a9a48560732d0c2f847112eed45ec0b49
Sergey68 <sesergey@yandex.ru>
2022-07-11 21:31:49 +1100

    File is updated with revised Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,21 +1,28 @@
 ﻿// цикл, который принимает на вход два числа (A и B) 
 //и возводит число A в натуральную степень B.
 
-int GetRate(double numA, double numB)
+double GetRate(double numA, double numB)
 {
-  double result = 1;
-  for(int j=1; j <= numB; j++)
-  {
-    double result = Math.Pow(numA, numB);
-  }
-  return result;
+    double result = 0;
+    if(numB == 0) 
+    {
+        result = 1;
+    }
+    if(numB > 0) 
+    {
+        result = numA;
+        for ( double i = 2; i<=numB; i++)
+        {
+            result = result * numA;
+        }
+    }
+    return result;
 }
-
 
   Console.Write("Enter number A: ");
   double numA = Convert.ToDouble(Console.ReadLine());
   Console.Write("Enter number B: ");
   double numB = Convert.ToDouble(Console.ReadLine());
 
-  int getrate = GetRate(numA, numB);
+  double getrate = GetRate(numA, numB);
   Console.Write("GetRate = " + getrate);
